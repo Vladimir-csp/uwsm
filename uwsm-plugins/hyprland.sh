7411daad6518923595c44fb2d1b3a31252882e5c
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2024-07-28 18:26:19 +0300

    fix!: apparently hyprland expects "1" value, "true" does not count

--- uwsm-plugins/hyprland.sh
+++ uwsm-plugins/hyprland.sh
@@ -15,7 +15,7 @@ quirks_hyprland() {
 	# https://github.com/hyprwm/Hyprland/commit/bd952dcef2ead3b0b7e2d730930a3fc528813ee0
 	# Without this unit will be declared started before "finalize" is executed,
 	# So some autostarted units may not get custom vars
-	export HYPRLAND_NO_SD_NOTIFY=true
+	export HYPRLAND_NO_SD_NOTIFY=1
 
 	# mark additional vars for export on finalize
 	UWSM_FINALIZE_VARNAMES="${UWSM_FINALIZE_VARNAMES}${UWSM_FINALIZE_VARNAMES+: }HYPRLAND_INSTANCE_SIGNATURE XCURSOR_SIZE XCURSOR_THEME"
