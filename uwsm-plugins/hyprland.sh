7e9660271e2a5b88ed46571a3d82d345ad274c17
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2024-08-09 10:29:20 +0300

    fix!: hyprland is no longer wlroots

--- uwsm-plugins/hyprland.sh
+++ uwsm-plugins/hyprland.sh
@@ -2,15 +2,6 @@
 # sourced by uwsm environment preloader
 
 quirks_hyprland() {
-	# append "wlroots" to XDG_CURRENT_DESKTOP if not already there
-	if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]
-	then
-		case "A:${XDG_CURRENT_DESKTOP}:Z" in
-		*:wlroots:*) true ;;
-		*) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
-		esac
-	fi
-
 	# Disable Hyprland's own systemd notification, supported sice:
 	# https://github.com/hyprwm/Hyprland/commit/bd952dcef2ead3b0b7e2d730930a3fc528813ee0
 	# Without this unit will be declared started before "finalize" is executed,
