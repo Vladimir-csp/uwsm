e3c9b5962b1e8f4b6aefa4f76a733e79b555f689
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2024-08-14 00:27:39 +0300

    fix: ensure titlecase "Hyprland" is among desktop names

--- uwsm-plugins/hyprland.sh
+++ uwsm-plugins/hyprland.sh
@@ -2,6 +2,15 @@
 # sourced by uwsm environment preloader
 
 quirks_hyprland() {
+	# append "Hyprland" to XDG_CURRENT_DESKTOP if not already there
+	if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]
+	then
+		case "A:${XDG_CURRENT_DESKTOP}:Z" in
+		*:Hyprland:*) true ;;
+		*) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:Hyprland" ;;
+		esac
+	fi
+
 	# Disable Hyprland's own systemd notification, supported sice:
 	# https://github.com/hyprwm/Hyprland/commit/bd952dcef2ead3b0b7e2d730930a3fc528813ee0
 	# Without this unit will be declared started before "finalize" is executed,
