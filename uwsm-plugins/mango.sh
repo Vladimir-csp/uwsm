489b90fe8c3695147004a3e1618a750a21f40e0a
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-02-14 14:30:21 +0200

    Cleaned up mangowc plugin
    Removed checking config for xwayland being disabled because you
    cannot explicitly disable it in mangowc (inherited from dwl).

--- uwsm-plugins/mango.sh
+++ uwsm-plugins/mango.sh
@@ -2,25 +2,16 @@
 # sourced by uwsm environment preloader
 
 quirks_mango() {
-
-  # detect disabled xwayland
-  if grep -qE '^[[:space:]]*xwayland[[:space:]]+disable' "${XDG_CONFIG_HOME}/mango/config.conf" 2> /dev/null; then
-	  XWAYLAND=false
-  else
-	  XWAYLAND=true
-  fi
-
   # Append "wlroots" to XDG_CURRENT_DESKTOP if not already there
-	# (mangowc is a wlroots-based compositor)
-	if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]; then
-	  case "A:${XDG_CURRENT_DESKTOP}:Z" in
-	  *:wlroots:*) true ;;
-	  *) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
-		esac
-	fi
+  # (mangowc is a wlroots-based compositor)
+  if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]; then
+    case "A:${XDG_CURRENT_DESKTOP}:Z" in
+    *:wlroots:*) true ;;
+    *) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
+    esac
+  fi
 
   # mark additional vars for export on finalize
   UWSM_FINALIZE_VARNAMES="${UWSM_FINALIZE_VARNAMES}${UWSM_FINALIZE_VARNAMES:+ }XCURSOR_SIZE XCURSOR_THEME"
   export UWSM_FINALIZE_VARNAMES
-
 }
