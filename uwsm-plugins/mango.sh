a0dc2fa86bece5c024fa9f75b8fe62a84059fb2d
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2026-02-14 19:51:17 +0300

    style: applied shfmt -s -sr

--- uwsm-plugins/mango.sh
+++ uwsm-plugins/mango.sh
@@ -2,16 +2,16 @@
 # sourced by uwsm environment preloader
 
 quirks_mango() {
-  # Append "wlroots" to XDG_CURRENT_DESKTOP if not already there
-  # (mangowc is a wlroots-based compositor)
-  if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]; then
-    case "A:${XDG_CURRENT_DESKTOP}:Z" in
-    *:wlroots:*) true ;;
-    *) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
-    esac
-  fi
+	# Append "wlroots" to XDG_CURRENT_DESKTOP if not already there
+	# (mangowc is a wlroots-based compositor)
+	if [ "${__WM_DESKTOP_NAMES_EXCLUSIVE__}" != "true" ]; then
+		case "A:${XDG_CURRENT_DESKTOP}:Z" in
+		*:wlroots:*) true ;;
+		*) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
+		esac
+	fi
 
-  # mark additional vars for export on finalize
-  UWSM_FINALIZE_VARNAMES="${UWSM_FINALIZE_VARNAMES}${UWSM_FINALIZE_VARNAMES:+ }XCURSOR_SIZE XCURSOR_THEME"
-  export UWSM_FINALIZE_VARNAMES
+	# mark additional vars for export on finalize
+	UWSM_FINALIZE_VARNAMES="${UWSM_FINALIZE_VARNAMES}${UWSM_FINALIZE_VARNAMES:+ }XCURSOR_SIZE XCURSOR_THEME"
+	export UWSM_FINALIZE_VARNAMES
 }
